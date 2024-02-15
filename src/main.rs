812b6e3e4352055a08b75e1555e26f7a230af835
hamishteagle <64635817+hamishteagle@users.noreply.github.com>
2024-02-15 19:39:46 +0000

    Add number comparison logic to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
@@ -16,5 +17,13 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    println!("You guessed: {guess}")
+    println!("You guessed: {guess}");
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Equal => println!("Correct!"),
+        Ordering::Greater => println!("Too big.."),
+        Ordering::Less => println!("Too small..."),
+    }
 }
